c4e5e970db0c489193921348427a210fb3b8c62d
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-01-24 22:44:16 +0900

    add 自ユーザのトークと他ユーザのトークを分けるロジック追加
    showにユーザ情報を保持、トーク更新時に更新者と比較して表示を分ける

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,13 +2,19 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "room_channel_#{message.room_id}", message: render_message(message)
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", read_message: read_render_message(message), write_message: write_render_message(message), write_user_id: message.user.id
   end
 
   private
-    def render_message(message)
+    def read_render_message(message)
       # ApplicationController.renderer.renderでは、deviceのwarden proxyが無視されるため、独自メソッドを使用
       # ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message, current_user: message.user })
-      ApplicationController.render_with_signed_in_user(message.user,partial: 'messages/message', locals: { message: message })
+      ApplicationController.render_with_signed_in_user(message.user, partial: 'messages/read_message', locals: { message: message })
+    end
+
+    def write_render_message(message)
+      # ApplicationController.renderer.renderでは、deviceのwarden proxyが無視されるため、独自メソッドを使用
+      # ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message, current_user: message.user })
+      ApplicationController.render_with_signed_in_user(message.user, partial: 'messages/write_message', locals: { message: message })
     end
 end
