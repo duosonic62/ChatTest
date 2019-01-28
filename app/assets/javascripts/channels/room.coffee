c0713659a0bdaa1e741728d11d475cfb459f9dce
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-01-28 13:38:26 +0900

    add room_channelへのテスト
    action-cable-testingの導入、room_channelのテスト追加。

--- app/assets/javascripts/channels/room.coffee
+++ app/assets/javascripts/channels/room.coffee
@@ -20,6 +20,9 @@ document.addEventListener 'turbolinks:load', ->
     speak: (message)->
       @perform 'speak', message: message
 
+    rejected: ->
+      @perform 'unsubscribed'
+
   $(document).on 'keypress', '[data-behavior~=room_speaker]', (event) ->
     if event.keyCode is 13 # return = send
       App.room.speak event.target.value
---
+++ spec/channels/room_channel_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe RoomChannel, type: :channel do
+  let(:message) { create(:message_to_bob) }
+  let(:user) { message.user }
+  let(:room) { message.room }
+
+  before do
+    # identifiersでコネクションをイニシャライズ
+    stub_connection user_id: user.id
+  end
+
+  context '#subscribes' do
+    it "room_idが存在しない場合はrejectすること" do
+      subscribe
+      expect(subscription).to be_rejected
+    end
+
+    it "room_idが存在する場合はストリーミングを開始すること" do
+      subscribe({'room_id': 42})
+
+      expect(subscription).to be_confirmed
+      # expect(streams).to include("room_channel_42")
+      expect(subscription).to have_stream_from("room_channel_42")
+    end
+  end
+
+  context '#speak' do
+    before do
+      # コネクションのスタブにcurrent_userメソッドを追加する
+      class ActionCable::Channel::ConnectionStub
+        def current_user
+          user
+        end
+      end
+    end
+
+    it "messageが作成されること" do
+      subscribe({'room_id': room.id})
+      allow(connection).to receive(:current_user).and_return(user)
+      expect{ perform :speak, message: 'new content' }.to change(Message, :count).by(1)
+    end
+  end
+end
